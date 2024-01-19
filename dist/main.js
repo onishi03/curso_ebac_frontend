3ae9277598960061afa2770c160181665f72bbe6
Rodrigo Onishi <rodrigo@teste.com>
2024-01-19 16:25:01 -0300

    exercicio alunos

--- dist/main.js
+++ dist/main.js
@@ -4,27 +4,21 @@ var alunos = [{
   aluno: 'Rodrigo',
   nota: 3
 }, {
-  aluno: 'Jiro',
-  nota: 6
-}, {
-  aluno: 'Flora',
-  nota: 9
+  aluno: 'Natalia',
+  nota: 7
 }, {
   aluno: 'Arthur',
   nota: 10
 }, {
-  aluno: 'Natalia',
-  nota: 7
-}, {
-  aluno: 'Ivete',
-  nota: 0
+  aluno: 'Flora',
+  nota: 9
 }];
-function alunosAprovados(arr) {
-  var aprovados = arr.filter(function (obj) {
-    return obj.nota >= 6;
+function alunosAprovados(lista_alunos) {
+  var aprovados = lista_alunos.filter(function (aluno) {
+    return aluno.nota >= 6;
   });
-  return aprovados.map(function (obj) {
-    return obj.aluno;
+  return aprovados.map(function (aluno) {
+    return aluno.aluno;
   });
 }
 console.log(alunosAprovados(alunos));
--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 const alunos = [
     { aluno: 'Rodrigo', nota: 3 },
-    { aluno: 'Jiro', nota: 6 },
-    { aluno: 'Flora', nota: 9 },
-    { aluno: 'Arthur', nota: 10 },
     { aluno: 'Natalia', nota: 7 },
-    { aluno: 'Ivete', nota: 0 },
+    { aluno: 'Arthur', nota: 10 },
+    { aluno: 'Flora', nota: 9 },
 ];
 
-function alunosAprovados(arr) {
-    const aprovados = arr.filter(obj => obj.nota >= 6);
-    return aprovados.map(obj => obj.aluno);
+function alunosAprovados(lista_alunos) {
+    const aprovados = lista_alunos.filter(aluno => aluno.nota >= 6);
+    return aprovados.map(aluno => aluno.aluno);
 }
 
 console.log(alunosAprovados(alunos))
