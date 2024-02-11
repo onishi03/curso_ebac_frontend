86a1c9cd7832ed7904e31331df3e4e1db3fb0066
Rodrigo Onishi <rodrigo@teste.com>
2024-02-10 21:16:47 -0300

    exercicio vue finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,94 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { reactive } from 'vue';
+
+  const estado = reactive ( {
+    operacao: '+',
+    numero1: 0,
+    numero2: 0,
+  })
+
+  const soma = () => {
+    return parseInt(estado.numero1) + parseInt(estado.numero2)
+  }
+
+  const subtracao = () => {
+    return estado.numero1 - estado.numero2
+  }
+
+  const divisao = () => {
+    return estado.numero1 / estado.numero2
+  }
+
+  const multi = () => {
+    return estado.numero1 * estado.numero2
+  }
+
+  const operacoes = () => {
+    const { operacao } = estado;
+    switch (operacao) {
+      case '+':
+        return soma();
+      case '-':
+        return subtracao();
+      case '/':
+        return divisao();
+      case 'x':
+        return multi();
+    }
+  }
+
+  const valorPadrao = () => {
+    if(estado.numero1 === '') {
+      estado.numero1 = 0
+    }
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>Calculadora</h1>
+    <label for="">Primeiro Numero</label>
+    <input type="number" required @keyup="evento => estado.numero1 = evento.target.value">
+    <label for="">Segundo Numero</label>
+    <input type="number" required @keyup="evento => estado.numero2 = evento.target.value">
+    <select @change="evento => estado.operacao = evento.target.value">
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="x">x</option>
+      <option value="/">/</option>
+    </select>
+    <span>Resultado : <span v-if="operacoes() >= 0">{{operacoes()}}</span> <span v-else>Preencha os campos</span> </span>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .container {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
+    padding: 35px;
+    max-width: 400px;
+    gap: 35px;
+    align-items: center;
+    background-color: blueviolet;
+    margin: 25vh auto;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  input {
+    padding: 8px;
+    width: 80%;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  label {
+    font-size: 24px;
   }
-}
+
+  select {
+    width: 50%;
+    padding: 8px;
+  }
+
+  span {
+    font-size: 24px;
+  }
 </style>
